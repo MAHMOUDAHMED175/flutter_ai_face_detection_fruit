78d2ccc509aa137cfb06d7685abf45f4130e226f
MAHMOUDAHMED175 <eng.mahmoudahmed176@gmail.com>
2024-03-13 13:12:07 +0200

    another programme

---
+++ lib/new_ai_project/main.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: $e.code\nError Message: $e.message');
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'tflite real-time detection',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: HomePage(cameras!),
+    );
+  }
+}
